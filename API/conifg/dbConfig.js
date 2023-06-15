afb28bdb737b2c53a347e747ecbabc89cff58f0c
Ghaniy Ihdi Yustisio <128980590+GhaniyYustisio@users.noreply.github.com>
2023-06-15 23:06:03 +0700

    Update dbConfig.js

--- API/conifg/dbConfig.js
+++ API/conifg/dbConfig.js
@@ -1,1 +1,8 @@
+const { Sequelize } = require("sequelize");
 
+const sequelize = new Sequelize("anakosehat", "root", "anakosehat", {
+  host: "34.101.218.62",
+  dialect: "mysql",
+});
+
+module.exports = sequelize;
