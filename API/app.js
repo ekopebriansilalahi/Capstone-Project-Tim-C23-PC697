8919703415fea617d97615d02b293a9b04c925f3
Ghaniy Ihdi Yustisio <128980590+GhaniyYustisio@users.noreply.github.com>
2023-06-15 23:49:08 +0700

    Update app.js

--- API/app.js
+++ API/app.js
@@ -1,14 +1,21 @@
-import express from "express";
-import router from "./server"; // Ganti "./router" dengan path file yang sesuai
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const calorieRoutes = require("./routes/calorieRoutes");
+const allergyRoutes = require("./routes/allergyRoutes");
+const foodPreferenceRoutes = require("./routes/foodPreferenceRoutes");
 
 const app = express();
 
-// Mengaitkan router dengan aplikasi utama
-app.use("/login", router.login);
-app.use("/server", router.server);
-app.use("/food", router.food); // Ganti "/food" dengan jalur yang sesuai
+app.use(bodyParser.json());
 
-// Menjalankan server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Routes
+app.use("/auth", authRoutes);
+app.use("/calorie", calorieRoutes);
+app.use("/allergy", allergyRoutes);
+app.use("/food-preference", foodPreferenceRoutes);
+
+// Start the server
+app.listen(3003, () => {
+  console.log("Server is running on port 3003");
 });
