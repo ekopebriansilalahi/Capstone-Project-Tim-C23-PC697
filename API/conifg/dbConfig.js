59d368378ae33eb1d1145c5b2d08b63ba93a3637
Ghaniy Ihdi Yustisio <128980590+GhaniyYustisio@users.noreply.github.com>
2023-06-15 23:06:59 +0700

    Delete API/conifg directory

--- API/conifg/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("anakosehat", "root", "anakosehat", {
-  host: "34.101.218.62",
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
