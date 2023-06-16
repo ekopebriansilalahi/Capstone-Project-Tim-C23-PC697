b16a2293b42b363f35380aa9eb3ceaf2aab619d7
albarrazikrillah14 <albarrazikrillah1405@gmail.com>
2023-06-16 14:15:41 +0700

    initial

---
+++ app/src/main/java/com/capstone/anakosehat/data/remote/ApiConfig.kt
@@ -0,0 +1,24 @@
+package com.capstone.anakosehat.data.remote
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object ApiConfig {
+    private val okHttp = OkHttpClient.Builder()
+        .apply {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
+            addInterceptor(loggingInterceptor)
+        }.build()
+
+    private val retrofit = Retrofit.Builder()
+        .client(okHttp)
+        .baseUrl("https://capstone-388811.et.r.appspot.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val hereAppService = retrofit.create<AnaKoSehatService>()
+}
---
+++ app/src/main/java/com/capstone/anakosehat/data/repository/AnaKoSehatRepository.kt
@@ -0,0 +1,24 @@
+package com.capstone.anakosehat.data.repository
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import com.capstone.anakosehat.data.model.PostSignUpRequest
+import com.capstone.anakosehat.data.model.Response
+import com.capstone.anakosehat.data.remote.AnaKoSehatService
+import com.capstone.anakosehat.utils.Result
+
+class AnaKoSehatRepository(private val apiService: AnaKoSehatService) {
+    fun postSignUp(request: PostSignUpRequest): LiveData<Result<Response>>  = liveData {
+
+        emit(Result.Loading)
+        try {
+            val response = apiService.postSignUp(request)
+            emit(Result.Success(response))
+
+        } catch (e: Exception) {
+            Log.d("REGISTER", e.message.toString())
+            emit(Result.Error(e.message.toString()))
+        }
+    }
+}
